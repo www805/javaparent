6e29b7e3689ab6616d5ef312ec5d289dde6dac0c
zjl <zjl@qq.com>
2020-01-16 11:37:54 +0800

    springboot集成其他框架No2，zuul、eureka

---
+++ springboot_zuul/src/main/java/com/zhuang/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.zhuang.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Auther: zhuang
+ * @Date: 2020/1/10 0010 16:33
+ * @Description:
+ */
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
